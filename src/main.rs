85cc7f2567a8eb9b43dd5743612daba79ca04bd4
superlogical <jake.net@gmail.com>
2014-09-13 15:07:11 +1200

    convert tabs to spaces

--- src/main.rs
+++ src/main.rs
@@ -7,45 +7,45 @@ use postgres::{PostgresConnection, NoSsl};
 use postgres::types::ToSql;
 
 struct Person {
-	id: i32,
-	name: String,
-	created: Timespec,
-	data: Option<Vec<u8>>
+    id: i32,
+    name: String,
+    created: Timespec,
+    data: Option<Vec<u8>>
 }
 
 fn main() {
-	let conn = PostgresConnection::connect("postgres://postgres:swine@localhost/rusty", &NoSsl).unwrap();
-	
-	conn.execute("DROP TABLE IF EXISTS person;", []).unwrap();
+    let conn = PostgresConnection::connect("postgres://postgres:swine@localhost/rusty", &NoSsl).unwrap();
+    
+    conn.execute("DROP TABLE IF EXISTS person;", []).unwrap();
 
-	conn.execute(		
-		"CREATE TABLE IF NOT EXISTS person (
-			id 			SERIAL PRIMARY KEY,
-			name		VARCHAR NOT NULL,
-			created		TIMESTAMP NOT NULL,
-			data		BYTEA
-		);", []).unwrap();
+    conn.execute(       
+        "CREATE TABLE IF NOT EXISTS person (
+            id          SERIAL PRIMARY KEY,
+            name        VARCHAR NOT NULL,
+            created     TIMESTAMP NOT NULL,
+            data        BYTEA
+        );", []).unwrap();
 
-	let person = Person {
-		id: 0,
-		name: "Jake Scott".to_string(),
-		created: time::get_time(),
-		data: None
-	};
+    let person = Person {
+        id: 0,
+        name: "Jake Scott".to_string(),
+        created: time::get_time(),
+        data: None
+    };
 
-	conn.execute("INSERT INTO person (name, created, data) VALUES ($1, $2, $3);",
-		&[&person.name, &person.created, &person.data]).unwrap();
+    conn.execute("INSERT INTO person (name, created, data) VALUES ($1, $2, $3);",
+        &[&person.name, &person.created, &person.data]).unwrap();
 
-	let stmt = conn.prepare("SELECT id, name, created, data FROM person;").unwrap();
+    let stmt = conn.prepare("SELECT id, name, created, data FROM person;").unwrap();
 
-	for row in stmt.query([]).unwrap() {
-		let p = Person {
-			id: row.get(0u),
-			name: row.get(1u),
-			created: row.get(2u),
-			data: row.get(3u)
-		};
-		
-		println!("Found person {}", p.name);
-	}
+    for row in stmt.query([]).unwrap() {
+        let p = Person {
+            id: row.get(0u),
+            name: row.get(1u),
+            created: row.get(2u),
+            data: row.get(3u)
+        };
+
+        println!("Found person {}", p.name);
+    }
 }
